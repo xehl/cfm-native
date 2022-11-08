5dc8cceb53b52bf8c70eb8aeeeaeb3fcaae631c1
ehlee95 <eric95lee@gmail.com>
2022-11-08 15:13:12 -0500

    updated dependencies

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import { registerRootComponent } from "expo";
-import { AppRegistry, Platform } from "react-native";
 import App from "./App";
+// import { AppRegistry, Platform } from "react-native";
 import TrackPlayer from "react-native-track-player";
 TrackPlayer.registerPlaybackService(() => require("./service"));
-// import { name as appName } from "./app.json";
-// AppRegistry.registerComponent(appName, () => MyApp);
 
 // AppRegistry.registerComponent("cfm", () => App);
-
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
@@ -16,6 +13,11 @@ registerRootComponent(App);
 // AppRegistry.registerComponent("main", () => App);
 
 // if (Platform.OS === "web") {
+//   const rootTag =
+//     document.getElementById("root") || document.getElementById("main");
+//   AppRegistry.runApplication("main", { rootTag });
+// }
+// if (Platform.OS === "ios") {
 //   const rootTag =
 //     document.getElementById("root") || document.getElementById("main");
 //   AppRegistry.runApplication("main", { rootTag });
